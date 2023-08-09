36775d6c29519fe48bc7419d46c487ec86817a2d
Jmingpt <88297237+Jmingpt@users.noreply.github.com>
2023-08-09 14:51:55 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,72 @@
+import streamlit as st
+import pandas as pd
+from cryptography.fernet import Fernet
+
+
+@st.cache_data
+def convert_df(df):
+    return df.to_csv(index=False).encode('utf-8')
+
+
+def fernet_decrypt(key, encrypted_message):
+    key = key.encode()
+    cipher = Fernet(key)
+    decrypted_message = cipher.decrypt(encrypted_message.encode())
+
+    return decrypted_message.decode()
+
+
+def run():
+    st.set_page_config(
+        page_title="Decryption Tool",
+        page_icon="🔑"
+    )
+    st.title("Decryption Tool")
+
+    with st.sidebar:
+        key = st.text_input(
+            label='Decryption Key',
+            value=''
+        )
+
+    uploaded_file = st.file_uploader(
+        label='Choose a CSV file',
+        type='csv',
+        accept_multiple_files=False
+    )
+    if uploaded_file:
+        data = pd.read_csv(uploaded_file).sample(500)
+        cols = data.columns.tolist()
+
+        if key != '':
+            nonencrypt_cols = []
+            for col in cols:
+                try:
+                    data[f'decrypted_{col}'] = data[col].apply(lambda x: fernet_decrypt(key, x))
+                except:
+                    nonencrypt_cols.append(col)
+
+            result_cols = nonencrypt_cols + [c for c in data.columns if c.startswith('decrypted')]
+            result = data[result_cols]
+            result.columns = [c.lower().replace(' ', '_') for c in result.columns]
+            st.dataframe(
+                data=result.head(10),
+                use_container_width=True,
+                hide_index=True
+            )
+
+            download_cols = st.columns((1, 2))
+            with download_cols[0]:
+                download_df = convert_df(result)
+                st.download_button(
+                    label="Download data as .csv",
+                    data=download_df,
+                    file_name='decrypted_file.csv',
+                    mime='text/csv',
+                )
+            with download_cols[1]:
+                st.markdown(f'Total rows of the file: **{result.shape[0]}**')
+
+
+if __name__ == "__main__":
+    run()
